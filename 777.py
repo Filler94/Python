682b548d581aa716f67cabcc2679bef03458a792
Filler94 <64613492+Filler94@users.noreply.github.com>
2020-04-30 16:39:57 -0400

    Add files via upload

---
+++ 777.py
@@ -0,0 +1,20 @@
+﻿secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+number_guess = int(input("Ener your guess: "))
+
+while number_guess != 777:
+    print("Ha ha! You're stuck in my loop!")
+    number_guess = int(input("Enter another guess: "))
+
+else:
+    print("Well done, muggle! You are free now.", number_guess)
